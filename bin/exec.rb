9cd51ff0408848f47ed2db4b8a1540798234b0b9
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-11-03 22:16:12 -0400

    Modify CLI to handle bad inputs

--- bin/exec.rb
+++ bin/exec.rb
@@ -16,28 +16,48 @@ if input == 1
   puts "Enter the number to be added"
   second_num = gets.strip.to_i
   sum = calc.add(first_num, second_num)
-  puts "Here is your sum: #{sum}"
+  if sum.class == "Fixnum"
+    puts "Here is your sum: #{sum}"
+  else
+    puts "Sorry, your sum isn't a number. Please try again."
+  end
 elsif input == 2
   puts "Enter a number"
   first_num = gets.strip.to_i
   puts "Enter the number to subtract"
   second_num = gets.strip.to_i
   difference = calc.subtract(first_num, second_num)
-  puts "Here is your difference: #{difference}"
+  if difference.class == "Fixnum"
+    puts "Here is your difference: #{difference}"
+  else
+    puts "Sorry, your difference isn't a number. Please try again."
+  end
 elsif input == 3
   puts "Enter a number"
   first_num = gets.strip.to_i
   puts "Enter the number to multiply by"
   second_num = gets.strip.to_i
   product = calc.multiply(first_num, second_num)
-  puts "Here is your product: #{product}"
+  if product.class == "Fixnum"
+    puts "Here is your product: #{product}"
+  else
+    puts "Sorry, your product isn't a number. Please try again."
+  end
 elsif input == 4
   puts "Enter a number"
   first_num = gets.strip.to_i
   puts "Enter the number to divide by"
   second_num = gets.strip.to_i
-  quotient = calc.divide(first_num, second_num)
-  puts "Here is your quotient: #{quotient}"
+  if second_num == 0
+    puts "Sorry, you can't divide by zero."
+  else
+    quotient = calc.divide(first_num, second_num)
+    if quotient.class == "Fixnum"
+      puts "Here is your quotient: #{quotient}"
+    else
+      puts "Sorry, your quotient isn't a number. Please try again."
+    end
+  end
 else
   puts "Whoops, that's not a valid input. Please try again."
 end
