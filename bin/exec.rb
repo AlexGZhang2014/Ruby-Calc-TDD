e737dc17503daedbd5e9abcc677c53e18f6f2b0a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-11-03 22:08:32 -0400

    Define the rest of the CLI

--- bin/exec.rb
+++ bin/exec.rb
@@ -11,11 +11,33 @@ puts "4 - Divide"
 input = gets.strip.to_i
 
 if input == 1
-  puts "Which numbers are you adding?"
+  puts "Enter a number"
   first_num = gets.strip.to_i
+  puts "Enter the number to be added"
   second_num = gets.strip.to_i
   sum = calc.add(first_num, second_num)
-  puts sum
+  puts "Here is your sum: #{sum}"
+elsif input == 2
+  puts "Enter a number"
+  first_num = gets.strip.to_i
+  puts "Enter the number to subtract"
+  second_num = gets.strip.to_i
+  difference = calc.subtract(first_num, second_num)
+  puts "Here is your difference: #{difference}"
+elsif input == 3
+  puts "Enter a number"
+  first_num = gets.strip.to_i
+  puts "Enter the number to multiply by"
+  second_num = gets.strip.to_i
+  product = calc.multiply(first_num, second_num)
+  puts "Here is your product: #{product}"
+elsif input == 4
+  puts "Enter a number"
+  first_num = gets.strip.to_i
+  puts "Enter the number to divide by"
+  second_num = gets.strip.to_i
+  quotient = calc.divide(first_num, second_num)
+  puts "Here is your quotient: #{quotient}"
 else
-  puts "done"
+  puts "Whoops, that's not a valid input. Please try again."
 end
